e58052d1705fd9f1b710226d5ad259f020002430
linaKalala <96391344+linaKalala@users.noreply.github.com>
2022-12-18 14:43:11 -0500

    Add files via upload

---
+++ ContactServiceTest.java
@@ -0,0 +1,14 @@
+package contact;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ContactTest.java
@@ -0,0 +1,14 @@
+package contact;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
